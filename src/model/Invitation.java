7530895d1ff2b920570d54e2abc8e8376122913b
Charles Cahyadi <charlescahyadi97@gmail.com>
2024-12-12 08:53:57 +0700

    add invitation model

---
+++ src/model/Invitation.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Invitation {
+
+	private int invitationId;
+	private int eventId;
+	private int userId;
+	private String invitationStatus;
+	private int invitationRole;
+	
+	public Invitation(int invitationId, int eventId, int userId, String invitationStatus, int invitationRole) {
+		this.invitationId = invitationId;
+		this.eventId = eventId;
+		this.userId = userId;
+		this.invitationStatus = invitationStatus;
+		this.invitationRole = invitationRole;
+	}
+
+	public int getInvitationId() {
+		return invitationId;
+	}
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public String getInvitationStatus() {
+		return invitationStatus;
+	}
+
+	public int getInvitationRole() {
+		return invitationRole;
+	}
+
+	public void setInvitationStatus(String invitationStatus) {
+		this.invitationStatus = invitationStatus;
+	}
+	
+	
+	
+
+}
--- src/view/GuestHomeView.java
+++ src/view/GuestHomeView.java
@@ -1,9 +1,42 @@
 package view;
 
+import java.util.Vector;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.Event;
 
-public class GuestHomeView extends VBox{
+public class GuestHomeView extends HBox{
 
+	private VBox leftVbox, rightVbox;
+	private TableView<Event> eventTable;
+	private Vector<Event> events;
+	private Label filterLbl, activeLbl;
+	private int id;
+	
+	
+	private void init() {
+		events = new Vector<Event>();
+		
+		leftVbox = new VBox();
+		rightVbox = new VBox();
+		eventTable = new TableView<Event>();
+		filterLbl = new Label();
+		activeLbl = new Label();
+	}
+	
+	private void setLayout() {
+		this.leftVbox.getChildren().addAll(filterLbl);
+		this.rightVbox.getChildren().addAll(activeLbl, eventTable);
+		this.getChildren().addAll(leftVbox, rightVbox);
+	}
+	
+	private void setTable() {
+		
+	}
+	
 	public GuestHomeView() {
 		
 	}
